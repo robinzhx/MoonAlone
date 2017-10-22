0958fa559c31eaf798a962c9a8f57a27d9c2e2a9
robinzhx <zhx068@ucsd.edu>
2017-10-22 09:20:19 -0700

    final push

--- Assets/DisplayOxygenLevel.cs
+++ Assets/DisplayOxygenLevel.cs
@@ -6,18 +6,24 @@ public class DisplayOxygenLevel : MonoBehaviour {
 
 	private SteamVR_TrackedObject trackedObject;
 	private SteamVR_Controller.Device device;
+    private SteamVR_TrackedController controller;
 
-	// Use this for initialization
-	void Start () {
-		trackedObject = GetComponent<SteamVR_TrackedObject> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Use this for initialization
+    void Start ()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.PadClicked += Controller_PadClicked;
+    }
+
+    private void Controller_PadClicked(object sender,ClickedEventArgs e)
+    {
+        if (device.GetAxis().x!=0|| device.GetAxis().y != 0)
+            Debug.Log(device.GetAxis().x + "" + device.GetAxis().y);
+    }
+    // Update is called once per frame
+    void Update () {
 		device = SteamVR_Controller.Input ((int)trackedObject.index);
-		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
-			Debug.Log ("Trigger Press");
-			device.TriggerHapticPulse (700);
-		}
 	}
 }
--- Assets/Scripts/pickup.cs
+++ Assets/Scripts/pickup.cs
@@ -1,22 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pickup : MonoBehaviour {
 
 	public Rigidbody rb;
+    public Text level;
+    private int oxygenLevel;
+    private int x = 0;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent < Rigidbody>();
-	}
+        oxygenLevel = 100;
+       
 
-	void onTriggerEnter(Collider other)
+    }
+    private void FixedUpdate()
+    {
+        x++;
+        if(x==700)
+        {
+            oxygenLevel -= 1;
+            x = 0;
+        }
+        setLevelText();
+    }
+    void OnTriggerEnter(Collider other)
 	{
-		Debug.Log("hit1");
 		if(other.gameObject.CompareTag("Pick Up"))
 		{
-			Debug.Log("hit");
 			other.gameObject.SetActive(false);
-		}
+            oxygenLevel += 10;
+            setLevelText();
+        }
 	}
+    void setLevelText()
+    {
+        if(oxygenLevel>100)
+        {
+            oxygenLevel = 100;
+        }
+        if (oxygenLevel > 0)
+        {
+            level.text = "O2: " + oxygenLevel.ToString();
+
+        }
+        else
+        {
+            level.text = "Oh no, you don't have any oxygen left. But give us a like to be back to full health";
+        }
 }
