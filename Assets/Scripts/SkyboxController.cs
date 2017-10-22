a4446560150f286afea2a76705f69dec3bd38718
robinzhx <zhx068@ucsd.edu>
2017-10-21 17:47:25 -0700

    Update body

---
+++ Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour {
+    private float rot;
+
+	// Use this for initialization
+	void Start () {
+        rot = RenderSettings.skybox.GetFloat("_Rotation");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rot -= 0.01f;
+        if (rot < 0.0f) rot = 360.0f;
+        RenderSettings.skybox.SetFloat("_Rotation", rot);
+    }
+}
