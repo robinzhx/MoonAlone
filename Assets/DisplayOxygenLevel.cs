8c507d0b413bf6d88ee9acf1a6142fff04d2660e
Guanxin <liguanxin078@gmail.com>
2017-10-22 00:10:17 -0700

    add pad sensor

---
+++ Assets/DisplayOxygenLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayOxygenLevel : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		device = SteamVR_Controller.Input ((int)trackedObject.index);
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger Press");
+			device.TriggerHapticPulse (700);
+		}
+	}
+}
